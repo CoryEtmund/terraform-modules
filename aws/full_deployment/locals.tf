729ca4b5db6da944360ce6d05c137be4e9a5dacf
Cory Etmund <Cory.Etmund@nreca.coop>
2025-06-18 12:45:22 -0500

    functioning deployments in us-east-1 and 2

---
+++ aws/full_deployment/locals.tf
@@ -0,0 +1,3 @@
+#locals {
+#  provider_alias = "aws.${var.region}"
+#}
--- aws/full_deployment/main.tf
+++ aws/full_deployment/main.tf
@@ -1,8 +1,43 @@
+#provider "aws" {
+#  region = "us-east-1"
+#  alias = "us-east-1"
+#}
+#
+#provider "aws" {
+#  region = "us-east-2"
+#  alias = "us-east-2"
+#}
+#
+#provider "aws" {
+#  region = "us-east-1"
+#  alias = "UE1"
+#}
+#
+#provider "aws" {
+#  region = "us-east-2"
+#  alias = "UE2"
+#}
+#
+#locals {
+#  provider_alias = "aws.${var.region}"
+#}
+
 provider "aws" {
-  region = var.region  
+  region = var.region
 }
 
 module "networking" {
   source = "../networking"
   vpc = var.vpc
+  #providers = {
+  #  aws = aws.us-east-1
+  #}
 }
+
+#module "networking2" {
+#  source = "../networking"
+#  vpc = var.vpc
+#  providers = {
+#    aws = aws.UE2
+#  }
+#}
